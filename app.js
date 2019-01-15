1c2b41eeb7d70d05355d62146ae4a30f45bc72aa
nightwing917@hotmail.com <nightwing917@hotmail.com>
2019-01-14 20:11:23 -0700

    trying to append li

--- app.js
+++ app.js
@@ -19,7 +19,28 @@ function loadEventListeners() {
 function addTask(e) {
     if (taskInput.value === '') {
         alert('Add a task');
-    }
+    } 
+
+    // Create li item
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Creat new link element
+    const link = doucment.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // Append link to li
+    li.appendChild(link);
+    // Append li to ul
+    taskList.appendChild(li);
+    // clear input
+    taskInput.value = '';
+
+    e.preventDefault();
 }
 
 
