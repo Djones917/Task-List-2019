5d206f713c2270c425c5832525bba2dc5cb53d3e
Derek <nightwing917@hotmail.com>
2019-01-18 18:53:58 -0700

    add font awesome x

--- app.js
+++ app.js
@@ -27,14 +27,14 @@ function addTask(e) {
     li.className = 'collection-item';
     // Create text node and append to li
     li.appendChild(document.createTextNode(taskInput.value));
-    // Creat new link element
-    //const link = doucment.createElement('a');
+    // Create new link element
+    const link = document.createElement('a');
     // Add class
-    //link.className = 'delete-item secondary-content';
+    link.className = 'delete-item secondary-content';
     // Add icon html
-    //link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="fas fa-times"></i>';
     // Append the link to li
-    //li.appendChild(link);
+    li.appendChild(link);
     // Append li to ul
     taskList.appendChild(li);
     // clear input
