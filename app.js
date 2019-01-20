d12fd75934ed25a59bc04ea1ae4b169831f6b8b1
Derek <nightwing917@hotmail.com>
2019-01-19 19:35:36 -0700

    finished filer

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ function loadEventListeners() {
     taskList.addEventListener('click', removeTask);
     // Clear task event
     clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -66,5 +68,20 @@ function clearTasks() {
         taskList.removeChild(taskList.firstChild);
     }
 } 
-// Delete this and console.log below just trying to figure out why debugger is not working!
-//console.log(addTask);
+
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+}
+
+
